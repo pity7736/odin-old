1320d08656d7dbe410332ff4c6fe6b6ce27de57f
Julián Cortés <pity7736@gmail.com>
2019-04-08 21:46:57 -0500

    test

---
+++ run_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo "Build cython modules..."
+compile_result="$(python setup.py build_ext)"
+if [ "$compile_result" == 'running build_ext' ]
+then
+    echo "Installing project locally..."
+    pip install -e .
+    echo "Running pytest..."
+    pytest -vvvv --cov=gideon --cov-report term-missing tests/
+fi
