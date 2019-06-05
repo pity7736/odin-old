617a99806e9c627c4ec4548e1250c1ff52085d82
Julián Cortés <pity7736@gmail.com>
2019-06-04 22:56:30 -0500

    use starlette instead of sanic.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
+import uvicorn
+
 
 if __name__ == '__main__':
     load_dotenv()
-    from odin import app
-    app.run(host='0.0.0.0', port='8888')
+    uvicorn.run(app='odin:app', host='0.0.0.0', port=8888, reload=True)
--- odin/api/api.py
+++ odin/api/api.py
@@ -1,26 +1,15 @@
 from graphql.execution.executors.asyncio import AsyncioExecutor
-from sanic import Sanic
-from sanic.response import json
-from sanic_graphql import GraphQLView
+from starlette.applications import Starlette
+from starlette.graphql import GraphQLApp
+from starlette.responses import UJSONResponse
 
 from .schema import schema
 
 
-app = Sanic()
-
-
-@app.listener('before_server_start')
-def init_graphql(app, loop):
-    app.add_route(
-        GraphQLView.as_view(
-            schema=schema,
-            executor=AsyncioExecutor(loop=loop),
-            graphiql=True
-        ),
-        '/api/'
-    )
+app = Starlette(debug=True)
+app.add_route('/api/', GraphQLApp(schema=schema, executor_class=AsyncioExecutor))
 
 
 @app.route('/')
 async def root(request):
-    return json({'hello': 'world'})
+    return UJSONResponse({'hello': 'world!'})
