dd1ba528652f374b7eb2f1077ebb0085937faa86
Julián Cortés <pity7736@gmail.com>
2019-06-05 22:06:05 -0500

    removed innecessary code

--- odin/api/schema.py
+++ odin/api/schema.py
@@ -6,7 +6,6 @@ from odin.accounting.models import Category, Movement, Event
 from odin.accounting.mutations.category import CreateCategoryMutation
 from odin.accounting.mutations.movement import CreateExpenseMutation
 from odin.accounting.schemas import CategoryObjectType, MovementObjectType, EventObjectType
-from odin.accounting.schemas.wallet import WalletObjectType
 
 
 class Query(graphene.ObjectType):
@@ -19,52 +18,30 @@ class Query(graphene.ObjectType):
     @staticmethod
     async def resolve_category(root, info, id):
         category = await Category.get(id=id)
-        if category:
-            return CategoryObjectType(id=category.id, name=category.name, description=category.description)
+        return category
 
     @staticmethod
     async def resolve_categories(root, info):
         categories = await Category.all()
         result = []
         for category in categories:
-            result.append(CategoryObjectType(id=category.id, name=category.name, description=category.description))
+            result.append(category)
         return result
 
     @staticmethod
     async def resolve_movement(root, info, id):
         movement = await Movement.get(id=id)
         if movement:
-            category = await movement.get_category()
-            return MovementObjectType(
-                id=movement.id,
-                type=movement.type,
-                date=movement.date,
-                value=movement.value,
-                note=movement.note,
-                category=CategoryObjectType(id=category.id, name=category.name, description=category.description)
-            )
+            await movement.get_category()
+            return movement
 
     @staticmethod
     async def resolve_expenses(root, info):
         expenses = await Movement.all_expenses()
         result = []
         for expense in expenses:
-            category, wallet = await asyncio.gather(expense.get_category(), expense.get_wallet())
-            result.append(MovementObjectType(
-                id=expense.id,
-                type=expense.type,
-                date=expense.date,
-                value=expense.value,
-                note=expense.note,
-                category=CategoryObjectType(id=category.id, name=category.name, description=category.description),
-                wallet=WalletObjectType(
-                    id=wallet.id,
-                    name=wallet.name,
-                    initial_balance=wallet.initial_balance,
-                    balance=wallet.balance,
-                    created=wallet.created
-                )
-            ))
+            await asyncio.gather(expense.get_category(), expense.get_wallet())
+            result.append(expense)
         return result
 
     @staticmethod
