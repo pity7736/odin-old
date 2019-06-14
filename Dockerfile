faa3d1b6c168e1675ae36d0f006ea7076c9a9491
Julián Cortés <pity7736@gmail.com>
2019-06-13 21:10:41 -0500

    install gideon from pip. Added unique constraint to category name

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.7
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
-WORKDIR /code
 COPY . /code/
+WORKDIR /code
 RUN pip install -r requirements_dev.txt
--- create_db.sql
+++ create_db.sql
@@ -8,7 +8,7 @@ BEGIN;
 
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(40) NOT NULL,
+  name VARCHAR(40) UNIQUE NOT NULL,
   description TEXT NOT NULL
 );
 CREATE INDEX categories_name_index ON categories(name);
