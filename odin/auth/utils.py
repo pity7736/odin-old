9aaa5113c242c388a0a54743d663462203bd59f1
Julián Cortés <pity7736@gmail.com>
2019-07-11 22:59:38 -0500

    fix make password

--- odin/auth/utils.py
+++ odin/auth/utils.py
@@ -7,4 +7,4 @@ def make_password(raw_password, salt=None):
     salt = salt or get_random_string()
     hashed = pbkdf2(password=raw_password, salt=salt)
     encoded = base64.b64encode(hashed)
-    return f'{salt}$sha512$100000${encoded.decode()}'
+    return f'{salt}$sha512$200000${encoded.decode()}'
--- tests/unit_tests/auth/test_util.py
+++ tests/unit_tests/auth/test_util.py
@@ -3,5 +3,5 @@ from odin.auth.utils import make_password
 
 def test_make_password():
     password = make_password(raw_password='Th1s is a Pa$sword', salt='test')
-    assert password == 'test$sha512$100000$J5Za93zpx6imqOXST9Gn8JupUukV8wVG0tvvkkpUlIi1SR' \
+    assert password == 'test$sha512$200000$J5Za93zpx6imqOXST9Gn8JupUukV8wVG0tvvkkpUlIi1SR' \
                        'SX2YELwIoQQBMvxb7wYmNQV4IwojPNn3IDoR5U9g=='
